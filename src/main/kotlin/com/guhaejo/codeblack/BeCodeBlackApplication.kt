4c0d8450906dee5b025ac325720c95156f4a00d0
Yugyeong Park <bonbonly7@naver.com>
2024-05-23 23:12:45 +0900

    initial commit

---
+++ src/main/kotlin/com/guhaejo/codeblack/BeCodeBlackApplication.kt
@@ -0,0 +1,11 @@
+package com.guhaejo.codeblack
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BeCodeBlackApplication
+
+fun main(args: Array<String>) {
+	runApplication<BeCodeBlackApplication>(*args)
+}
---
+++ src/test/kotlin/com/guhaejo/codeblack/BeCodeBlackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.guhaejo.codeblack
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BeCodeBlackApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
